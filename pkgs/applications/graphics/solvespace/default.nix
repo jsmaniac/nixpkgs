d6f925d7c1ce29c2099e10dcbdb10628c1c63be1
Georges Dupéron <georges.duperon@gmail.com>
2017-03-17 17:04:56 +0100

    Updated solvespace

--- pkgs/applications/graphics/solvespace/default.nix
+++ pkgs/applications/graphics/solvespace/default.nix
@@ -1,26 +1,53 @@
-{ stdenv, fetchgit, autoreconfHook, fltk13
-, libjpeg, libpng, mesa, pkgconfig }:
+{ stdenv, fetchgit, cmake, fltk13
+, zlib, libjpeg, libpng, cairo, freetype
+, json_c, fontconfig, gtkmm3, pangomm, mesa, libspnav
+, pcre, libpthreadstubs, libXdmcp, libX11, libxkbcommon, epoxy, at_spi2_core, dbus, gettext
+, pkgconfig
+}:
 
 stdenv.mkDerivation {
-  name = "solvespace-2.0";
+  name = "solvespace-3.0";
   src = fetchgit {
-    url = "https://github.com/jwesthues/solvespace.git";
-    sha256 = "0m6zlx1kiqxkm6szdsnywwr6spnb7xjg6vqsq30nrr44cx37w861";
-    rev = "e587d0e";
+    url = "https://github.com/solvespace/solvespace.git";
+          #https://github.com/jwesthues/solvespace.git";
+    sha256 = "1n3p6zic1w5fvw85af8pl5mhmfqai67pd0dfr1qanbanydv4xh9c";
+    rev = "911c67b";
+    leaveDotGit = true;
   };
 
   # e587d0e fails with undefined reference errors if make is called
   # twice. Ugly workaround: Build while installing.
-  dontBuild = true;
+  #dontBuild = true;
   enableParallelBuilding = false;
 
   buildInputs = [
-    autoreconfHook
+    cmake
     fltk13
+    zlib
     libjpeg
     libpng
+    cairo
+    freetype
+    json_c
+    fontconfig
+    gtkmm3
+    pangomm
     mesa
+    libspnav
+    pcre
+    libpthreadstubs
+    libXdmcp
+    libX11
     pkgconfig
+    libxkbcommon
+    epoxy
+    at_spi2_core
+    dbus
+    gettext
+  ];
+
+  NIX_CFLAGS_COMPILE = [
+  "-I${pangomm}/lib/pangomm-1.4/include"
   ];
 
   meta = {
